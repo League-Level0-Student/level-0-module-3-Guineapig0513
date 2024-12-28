49384c86f6f9a84d03ee3d99e27fb6e7a2714945
league <league@imac-24.attlocal.net>
2024-12-27 16:56:53 -0800

    hi uh one game

--- src/_01_else_if/_5_own_adventure/AdventureCodeYipee.java
+++ src/_01_else_if/_5_own_adventure/AdventureCodeYipee.java
@@ -1,10 +1,26 @@
 package _01_else_if._5_own_adventure;
 
+import javax.swing.JOptionPane;
+
 public class AdventureCodeYipee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String name = JOptionPane.showInputDialog("Whats your name? :D (character, doesnt need to be ur name!)");
+	//	JOptionPane.showMessageDialog(null, "Hello "+name+ " a fellow travelor. My name is Sammy. Would you like to travel with me?");
+	//	JOptionPane.showInputDialog("Yes or no?");
+		int yesOrNo = JOptionPane.showConfirmDialog(null, "Hello "+name+ " a fellow travelor. My name is Sammy. Would you like to travel with me?", "", JOptionPane.YES_NO_OPTION);
+		if(yesOrNo == 1) {
+			JOptionPane.showMessageDialog(null, "Sorry for bothering you "+name+ ". Have safe travels fellow travelor! :)");
+			System.exit(0);
+		}
+		else  {
+			JOptionPane.showMessageDialog(null, "Ok lets go travel together "+name+"!");
+		}
+		int uh = JOptionPane.showOptionDialog(null, "message", "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, 
+			 new String[] {"Option1", "Option2"}, null);
+		
+		//JOptionPane.showOptionDialog(null, name, name, 0, 0, null, args, name)
 	}
-
 }
+
+
